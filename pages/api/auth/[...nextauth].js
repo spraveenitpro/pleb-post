d603a13205f4c64df1483e939887a2f32bb5f39e
Praveen <spraveenitpro@gmail.com>
2023-03-13 20:12:20 -0700

    Setting up authentication via github - working

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,31 +3,32 @@ import GithubProvider from "next-auth/providers/github"
 import axios from "axios"
 
 export const authOptions = {
-	// Configure one or more authentication providers
-	providers: [
-		GithubProvider({
-			clientId: process.env.GITHUB_ID,
-			clientSecret: process.env.GITHUB_SECRET,
-		}),
-		// ...add more providers here
-	],
-	callbacks: {
-		async session({ session }) {
-			console.log("session", session)
-			const user = await axios.post("http://localhost:3000/api/users", {
-				username: session.user.name,
-			});
-			if (user.status === 200) {
-				session.user = user.data.exists;
-				return session
-			} else if (user.status === 201) {
-				session.user = user.data;
-				return session
-			} else {
-				return session
-			}
-		}
-	}
+    // Configure one or more authentication providers
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        // ...add more providers here
+    ],
+    callbacks: {
+        async session({ session }) {
+            //console.log("session", session)
+            /* const user = await axios.get("http://localhost:3000/api/users", {
+                username: session.user.name,
+            });
+            if (user.status === 200) {
+                session.user = user.data.exists;
+                return session
+            } else if (user.status === 201) {
+                session.user = user.data;
+                return session
+            } else {
+                return session
+            } */
+            return session
+        }
+    }
 }
 
 export default NextAuth(authOptions)
